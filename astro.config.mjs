f28f72af249aa29e9ccf36099ab72378717acc3c
alifzaidan99 <alifzaidan99@gmail.com>
2026-07-17 18:21:51 +0700

    enhance code section and added passcode writeup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,37 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import expressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [
+        expressiveCode({
+            themes: ['material-theme-ocean'],
+
+            plugins: [pluginLineNumbers()],
+
+            defaultProps: {
+                showLineNumbers: true,
+            },
+
+            styleOverrides: {
+                codeFontFamily: "'Fira Code', monospace",
+                codeFontSize: '0.85rem',
+                codeBackground: '#282c34',
+            },
+        }),
+
+        mdx(),
+    ],
     vite: {
         plugins: [tailwindcss()],
-        server:{
-            allowedHosts: "3305-103-110-9-228.ngrok-free.app"
-        }
+        server: {
+            allowedHosts: '3305-103-110-9-228.ngrok-free.app',
+        },
     },
-    site: 'https://zensynthysian.github.io', 
-    base: '/kaleidoscape', 
+
+    site: 'https://zensynthysian.github.io',
+    base: '/kaleidoscape',
 });
